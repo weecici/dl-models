fix(transformer): handle the subsequent masking and pad masking properly

src/dl_models/transformer/_attn.py
@@ -74,11 +74,11 @@ class MultiHeadAttention(nn.Module):
                 mask = mask.unsqueeze(1).unsqueeze(2)  # -> [B, 1, 1, L2]
             elif mask.dim() == 3:  # [B, L1, L2]
                 mask = mask.unsqueeze(1)  # -> [B, 1, L1, L2]
-            # mask True = keep, False = mask out
+            # True = mask, False = no mask
             if mask.dtype == torch.bool:
-                attn_scores = attn_scores.masked_fill(~mask, -1e9)
+                attn_scores = attn_scores.masked_fill(mask, -1e9)
             else:
-                attn_scores = attn_scores.masked_fill(mask == 0, -1e9)
+                attn_scores = attn_scores.masked_fill(mask == 1, -1e9)
 
         # calculate attention probabilities using softmax on the last dimension, shape: [B, H, L1, L2]
         attn_probs = F.softmax(attn_scores, dim=-1)

src/dl_models/transformer/model.py
@@ -7,6 +7,7 @@ from torch import Tensor, LongTensor
 
 from .layer import EncoderLayer, DecoderLayer
 from ._pe import PositionalEncoding
+from ._utils import get_pad_mask, get_subsequent_mask
 from ._const import *
 
 
@@ -20,11 +21,12 @@ class Encoder(nn.Module):
         dropout: float,
         max_len: int,
         num_layers: int,
+        pad_idx: int,
     ):
         super().__init__()
         self.scalar = math.sqrt(d_model)
 
-        self.embedding = nn.Embedding(vocab_size, d_model)
+        self.embedding = nn.Embedding(vocab_size, d_model, padding_idx=pad_idx)
         self.pos_encoding = PositionalEncoding(d_model, max_len, dropout=dropout)
         self.layer_stack = nn.ModuleList(
             [EncoderLayer(d_model, d_ff, num_heads, dropout) for _ in range(num_layers)]
@@ -53,11 +55,12 @@ class Decoder(nn.Module):
         dropout: float,
         max_len: int,
         num_layers: int,
+        pad_idx: int,
     ):
         super().__init__()
         self.scalar = math.sqrt(d_model)
 
-        self.embedding = nn.Embedding(vocab_size, d_model)
+        self.embedding = nn.Embedding(vocab_size, d_model, padding_idx=pad_idx)
         self.pos_encoding = PositionalEncoding(d_model, max_len, dropout=dropout)
         self.layer_stack = nn.ModuleList(
             [DecoderLayer(d_model, d_ff, num_heads, dropout) for _ in range(num_layers)]
@@ -93,14 +96,29 @@ class Transformer(nn.Module):
         max_len: int = DEFAULT_MAX_LEN,
         num_encoder_layers: int = DEFAULT_NUM_LAYERS,
         num_decoder_layers: int = DEFAULT_NUM_LAYERS,
+        pad_idx: int = DEFAULT_PAD_IDX,
     ):
         super().__init__()
 
         self.encoder = Encoder(
-            vocab_size, d_model, d_ff, num_heads, dropout, max_len, num_encoder_layers
+            vocab_size,
+            d_model,
+            d_ff,
+            num_heads,
+            dropout,
+            max_len,
+            num_encoder_layers,
+            pad_idx=pad_idx,
         )
         self.decoder = Decoder(
-            vocab_size, d_model, d_ff, num_heads, dropout, max_len, num_decoder_layers
+            vocab_size,
+            d_model,
+            d_ff,
+            num_heads,
+            dropout,
+            max_len,
+            num_decoder_layers,
+            pad_idx=pad_idx,
         )
         self.linear = nn.Linear(d_model, vocab_size, bias=False)
 
@@ -108,13 +126,20 @@ class Transformer(nn.Module):
         self.linear.weight = self.encoder.embedding.weight
         self.decoder.embedding.weight = self.encoder.embedding.weight
 
+        self.pad_idx = pad_idx
+
     def forward(
         self,
         src: LongTensor,
         tgt: LongTensor,
-        src_mask: Tensor | None = None,
-        tgt_mask: Tensor | None = None,
     ) -> Tensor:
+        src_mask = get_pad_mask(src, self.pad_idx)
+        tgt_mask = get_pad_mask(tgt, self.pad_idx) & get_subsequent_mask(tgt)
+
+        assert src.dim() == tgt.dim() == 2, "Input tensors must be 2-dimensional"
+        assert (
+            src.shape[0] == tgt.shape[0]
+        ), "Input tensors must have the same batch size"
 
         enc_out = self.encoder(src, src_mask)
         dec_out = self.decoder(tgt, enc_out, tgt_mask, src_mask)